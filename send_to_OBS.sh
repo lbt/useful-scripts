b1fee521cf255963de7b5b699f0c1db02eae4fa3
David Greaves <david@dgreaves.com>
2011-08-05 15:32:15 +0000

    Verify the git tree is clean. Resolve some issues around project cleanup Document the gem publish process

--- send_to_OBS.sh
+++ send_to_OBS.sh
@@ -2,6 +2,34 @@
 
 set -e
 
+require_clean_work_tree () {
+    # Update the index
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    # Disallow unstaged changes in the working tree
+    if ! git diff-files --quiet --ignore-submodules --
+    then
+        echo >&2 "cannot $1: you have unstaged changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        err=1
+    fi
+
+    # Disallow uncommitted changes in the index
+    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        echo >&2 "cannot $1: your index contains uncommitted changes."
+        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        echo >&2 "Please commit or stash them."
+        exit 1
+    fi
+}
+
 usage()
 {
     cat <<EOF
@@ -11,8 +39,13 @@ usage()
 
 git should be setup to have a pristine branch and a packaging branch
 There should be a debian/gbp.conf file specifing
-debian-branch     (typically...)
+debian-branch     (typically pkg or debian)
 upstream-branch   (typically master)
+
+You should be on the pkg branch to build a proper build
+
+If you are on any other branch then the current sha1 will be appended
+to the Release.
 
 EOF
     return 0
@@ -28,7 +61,11 @@ BUILDAREA=/some/tmp/build-area
 # We need to be in a git repo
 [[ ! -d .git ]] && { echo Not in a .git project ; exit 1 ; }
 
+require_clean_work_tree
+
 BRANCH=$(git status -bs | grep '##' | cut -d' ' -f2)
+
+HEAD=$(git show-ref -s HEAD --abbrev)
 
 while getopts "p:" opt; do
     case $opt in
@@ -117,14 +154,12 @@ fi
 
 echo "################################################################"
 echo Sending to OBS
+
+# Update to ensure we can overwrite - git is master
+(cd $OBSDIR; osc up)
+
 # Build succeeded - clean out the OBS dir and use new tarballs
 rm -f $OBSDIR/*
-
-# is there a gem here?
-if [[ -n "$(find . -maxdepth 1 -name '*.gem' -print -quit)" ]]; then
-    rm -f $OBSDIR/*gem
-fi
-
 
 if [[ $GBP == "yes" ]]; then
     mv $BUILD/* $OBSDIR/
@@ -142,9 +177,6 @@ cp rpm/* $OBSDIR/ 2>/dev/null || true
 mv *.gem $OBSDIR/ 2>/dev/null || true
 mv pkg/*.gem $OBSDIR/ 2>/dev/null || true
 
-# Update to ensure we can overwrite - git is master
-(cd $OBSDIR; osc up)
-
 # Send it to the OBS (should this use the changelog entry?)
 dpkg-parsechangelog -c1 | (
     cd $OBSDIR
@@ -153,3 +185,25 @@ dpkg-parsechangelog -c1 | (
     osc ar
     osc ci
 )
+
+# Debian Package Dependencies
+# git-buildpackage
+# gem2deb
+
+# Using send_to_OBS.sh to update a gem
+######################################
+# Checkout master
+# pull from remote
+# git pull master
+# git checkout debian
+# git merge master
+# Go to debian/
+## Edit changelog, increase version
+## include upstream change comments
+## Update control if Rakefile changed deps
+# Go to rpm/
+## Manually update rpm Version
+## include upstream change comments
+# Commit changelog/spec changes
+# Stay on debian/pkg branch
+# run /maemo/devel/BOSS/send_to_OBS.sh -p home:lbt:MINT
